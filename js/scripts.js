4edbd94d2a4fcef4d4a2d770eec7ba8453c595d7
Travis Crampton <crampton.travis@gmail.com>
2015-09-11 09:43:30 -0700

    run the program and using find and replace with the replace method.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,17 @@ var findReplace = function(sentence, find, replace) {
   var newSentence = sentence.replace(find, replace);
   return newSentence;
 };
+
+$(document).ready(function() {
+  $("form#find_replace").submit(function(event) {
+    var sentence = $("input#sentence").val();
+    var find = $("input#find").val();
+    var replace = $("input#replace").val();
+    var result = findReplace(sentence, find, replace);
+
+    $(".newSentence").text(result);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
